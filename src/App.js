a1dad89f362ea9b413355859c289918f69b4e896
Kyle Schiller <kyleschiller@MacBook-Pro-2.local>
2021-08-02 15:18:37 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="grid">
+      <div class="box gradient-1"></div>
+      <div class="box gradient-2">
+        <div class="gradient-2-inner"></div>
+      </div>
+      <div class="box gradient-3">
+        <div class="gradient-3-inner"></div>
+        <div class="gradient-3-inner-2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
